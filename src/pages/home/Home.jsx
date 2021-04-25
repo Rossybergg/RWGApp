3fe0ca5260e2de5cc09138689a664897d8ec42fa
Ross Hunter <44700365+Rossybergg@users.noreply.github.com>
2021-04-25 02:01:04 +0100

    updated intro video

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './home.scss';
 import { Paper, Typography} from "@material-ui/core";
 import logo from '../../assets/images/logo.svg'
 import Button from "@material-ui/core/Button";
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 function Home(props) {
+
+    const [loadingVideo, setLoadingVideo] = useState(true);
+
+    useEffect(() => {
+        const vid = document.getElementById("video");
+        vid.onloadeddata = () => {
+            setLoadingVideo(false);
+        };
+    }, [])
 
     const goToDiscord = () => {
         const url = 'http://www.discord.gg/hBxWqdh';
@@ -14,18 +24,24 @@ function Home(props) {
     return (
         <div className="Home">
             <Paper className="Paper">
+                { loadingVideo ?
+                    <div className="progressContainer">
+                        <LinearProgress className="progressBar" color="secondary" />
+                        <Typography id="bannerText" variant="body2">Loading...</Typography>
+                    </div>
+                    : null
+                }
                 <div id="logoContainer">
                     <img src={logo} alt="Red Wine Gaming" id="logo"/>
                 </div>
-                <video playsInline autoPlay muted loop className="video">
-                    <source src="https://r1---sn-aigl6nl7.c.drive.google.com/videoplayback?expire=1619322767&ei=T6-EYPT9MLucwtQPvNGk0AU&ip=82.5.202.12&cp=QVRGQURfUVdUSFhPOnBiMWRoLVBCNHhFZkpEdlpZRnE2LWljX1BtSWVLMnJ5TXl3ZHhNNU5vR20&id=44587b7fe5dc526a&itag=22&source=webdrive&requiressl=yes&mh=X1&mm=32&mn=sn-aigl6nl7&ms=su&mv=u&mvi=1&pl=22&ttl=transient&susc=dr&driveid=1rAD7EnLbp6-Zc9HQWaIOpgPDzi_OioGL&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=15.487&lmt=1619308078576995&mt=1619307999&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRQIhAMgrr-DOUoQWYNIFH8fCbwxAOOlzSbPdOrclc1sg4CVyAiAeAIF4-hee8ge7blAwXDJFWbcDjXGYvw2FZVYZ6F5Ftw==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgQdHegL-VYsrM7FHT2Hu86A1Yi9Z-gyC6eq4Jz47v-OoCIE3Gnipbupl7kBiFAXJ7jxDLGkFsRpoafw6cvon2I26h&cpn=NkAVxN5D6ZIVZ_XQ&c=WEB_EMBEDDED_PLAYER&cver=1.20210421.1.0" type="video/mp4"/>
-                            Your browser does not support video
-                </video>
                 <div className="BannerContainer">
                     <Typography id="bannerText" variant="h4">Coming Soon</Typography>
                 </div>
                 <Typography variant="body1">Welcome to Red Wine Gaming</Typography>
                     <Typography variant="body2">Our site is still a little far off but you can come and join us on discord!</Typography>
+                <video autoPlay muted loop className="video" id="video">
+                    <source src="https://drive.google.com/uc?export=download&id=1rAD7EnLbp6-Zc9HQWaIOpgPDzi_OioGL" type='video/mp4'/>
+                </video>
                 <div className="ButtonContainer">
                     <Button variant="contained"  color="primary" onClick={goToDiscord}>
                         Take me to Discord!
