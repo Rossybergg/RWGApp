45fbb321c202f1ef3228483a8d281b99848754f6
Ross Hunter <44700365+Rossybergg@users.noreply.github.com>
2021-06-15 22:20:49 +0100

    added css for chat componment

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,18 +1,82 @@
 import React from 'react';
 import './chat.scss'
+import Avatar from '@material-ui/core/Avatar';
+import defaultImage from '../../assets/images/defaultUser.jpg';
+import hangoutChatHeaderImage from '../../assets/images/hangout.jpg'
 
 
-const Chat = () => {
+const Chat = (props) => {
+
+    const me = '500376050103222273';
+
+    const formatMessages = (message) => {
+
+        if (message.userId !== me) {
+            return(
+                <div className="individual-message-container">
+                    <div className="name-and-date-container">
+                        <section className="inline-left">
+                            <Avatar alt={message.userName}
+                                    className="display-pic"
+                                    title={message.userName}
+                                    src={message.avatar ? `https://cdn.discordapp.com/avatars/${message.userId}/${message.avatar.toString()}` : defaultImage}
+                            />
+                        </section>
+                        <section className="inline-left name-left">
+                            {message.userName}
+                        </section>
+                        <section className="inline-left date-left">
+                            {message.timeStamp}
+                        </section>
+                    </div>
+                    <br className="clear-both"/>
+                    <div className="comment-bubble-left">
+                        <p>{message.message}</p>
+                    </div>
+                </div>
+            )
+        } else {
+            return(
+                <div className="individual-message-container">
+                    <div className="name-and-date-container">
+                        <section className="inline-right">
+                            <Avatar alt={message.userName}
+                                    className="display-pic"
+                                    title={message.userName}
+                                    src={message.avatar ? `https://cdn.discordapp.com/avatars/${message.userId}/${message.avatar.toString()}` : defaultImage}
+                            />
+                        </section>
+                        <section className="inline-right name-right">
+                            {message.userName}
+                        </section>
+                        <section className="inline-right date-right">
+                            {message.timeStamp}
+                        </section>
+                    </div>
+                    <br className="clear-both"/>
+                    <div className="comment-bubble-right">
+                        <p>{message.message}</p>
+                    </div>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="chat">
             <div className="header">
-                header
+                <img src={hangoutChatHeaderImage} alt="Hangout Chat" className="header-image"/>
             </div>
             <div className="messages">
-                messages go here
+                {props.messages.map((message) => formatMessages(message))}
             </div>
             <div className="compose">
-                you can send messages from here
+                <div className="send-title">
+                    Send Message
+                </div>
+                <div className="send-box">
+                  Add Message....
+                </div>
             </div>
         </div>
     )
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -9,6 +9,44 @@ import Chat from "../../components/Chat/Chat";
 
 function Quiz(props) {
 
+    const messages = [
+        {
+            userId: '500376050103222273',
+            avatar: 'a_28d5a787578797c40f1c7e16c160e0ce',
+            userName: 'Rossybergg',
+            message: 'All ya\'ll going down, theres only 1 king quizzer in town!!!',
+            timeStamp: '01/02/2020 14:30'
+        },
+        {
+            userId: '500376050103222272',
+            avatar: '',
+            userName: 'Test User',
+            message: 'Naaa we are gonna win!!!',
+            timeStamp: '01/02/2020 14:31'
+        },
+        {
+            userId: '500376050103222272',
+            avatar: '',
+            userName: 'Test User',
+            message: 'In what planet is an ant-eater not a carnivore!!!',
+            timeStamp: '01/02/2020 14:32'
+        },
+        {
+            userId: '500376050103222273',
+            avatar: 'a_28d5a787578797c40f1c7e16c160e0ce',
+            userName: 'Rossybergg',
+            message: 'I know right!!!! 🤬',
+            timeStamp: '01/02/2020 14:33'
+        },
+        {
+            userId: '500376050103222272',
+            avatar: '',
+            userName: 'Test User',
+            message: 'Em..... it eats ants.... not meat 🙄',
+            timeStamp: '01/02/2020 14:34'
+        },
+    ]
+
     return (
         <div className="Quiz">
             <Paper className="paper-quiz">
@@ -31,7 +69,7 @@ function Quiz(props) {
                 </div>
             </Paper>
             <div className="chat-container">
-                <Chat/>
+                <Chat messages={messages}/>
             </div>
         </div>
     )
